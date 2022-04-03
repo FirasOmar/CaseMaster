28fda59b8a70b16d9ad0854c7626a47ee42e85c9
Firas Omar <firasomar.1993@gmail.com>
2022-04-03 17:21:47 +0300

    configure repository

---
+++ CaseMaster/CaseMaster/Manager/ProductManager.cs
@@ -0,0 +1,20 @@
+﻿using CaseMaster.Data;
+using CaseMaster.Interfaces.Manager;
+using CaseMaster.Models;
+using CaseMaster.Repository;
+using EF.Core.Repository.Interface.Repository;
+using EF.Core.Repository.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaseMaster.Manager
+{
+    public class ProductManager : CommonManager<Organization>, IOrganizationManager
+    {
+        public ProductManager(AppDBContext dbContext) : base(new OrganizationRepository(dbContext))
+        {
+        }
+    }
+}
---
+++ CaseMaster/CaseMaster/Repository/OrganizationRepository.cs
@@ -0,0 +1,18 @@
+﻿using CaseMaster.Interfaces.Reporsitory;
+using CaseMaster.Models;
+using EF.Core.Repository.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaseMaster.Repository
+{
+    public class OrganizationRepository : CommonRepository<Organization>, IOrganizationRepository
+    {
+        public OrganizationRepository(DbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
