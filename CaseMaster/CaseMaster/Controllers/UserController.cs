cdc90529301d607ddfe5300a66f022e9468a8f37
Firas Omar <firasomar.1993@gmail.com>
2022-04-06 21:16:34 +0300

    edit and delete user

--- CaseMaster/CaseMaster/Controllers/UserController.cs
+++ CaseMaster/CaseMaster/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using CaseMaster.Manager;
+﻿using CaseMaster.Data;
+using CaseMaster.Manager;
 using CaseMaster.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -20,6 +21,79 @@ namespace CaseMaster.Controllers
         {
             var users = _ApplicationUserManager.GetAll();
             return View(users);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(UserViewModel userVM)
+        {
+            string msg = "";
+            userVM.Created = DateTime.Now;
+            userVM.CreatedBy = User.Identity.Name;
+            bool isSaved = _ApplicationUserManager.Add(userVM);
+            if (isSaved)
+            {
+                msg = "saved successfuly.";
+                return RedirectToAction("index");
+            }
+            else
+            {
+                msg = "Failed to Save.";
+            }
+            ViewBag.Msg = msg;
+            return View();
+        }
+        public IActionResult Edit(string id)
+        {
+            var user = _ApplicationUserManager.GetFirstOrDefault(c => c.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(UserViewModel user)
+        {
+         
+
+            var isUpdated = _ApplicationUserManager.Update(user);
+            if (isUpdated)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+        public ActionResult Details(string id)
+        {
+            var user = _ApplicationUserManager.GetFirstOrDefault(c => c.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+        public ActionResult Delete(string id)
+        {
+            var user = _ApplicationUserManager.GetFirstOrDefault(c => c.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult Delete(UserViewModel user)
+        {
+            bool isDeleted = _ApplicationUserManager.Delete(user);
+            if (isDeleted)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(user);
         }
     }
 }
